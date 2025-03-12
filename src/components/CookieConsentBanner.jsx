b4fc07d6cd27859cdecc371371e9a3d47be702ae
Shiv <shiv.choudhury@giva.co>
2025-03-12 09:09:18 +0530

    UI: Cookie banner improvemnet

--- src/components/CookieConsentBanner.jsx
+++ src/components/CookieConsentBanner.jsx
@@ -1,3 +1,4 @@
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import React, { useState, useEffect } from "react";
 
 const CookieConsentBanner = () => {
@@ -28,7 +29,7 @@ const CookieConsentBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white py-4 px-6 flex flex-col md:flex-row items-center justify-between z-50 shadow-lg">
+    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 opacity-80 text-white py-4 px-6 flex flex-col md:flex-row items-center justify-between z-50 shadow-lg">
       <div className="mb-4 md:mb-0 md:mr-4 text-sm md:text-base">
         We use essential and functional cookies on our website to provide you
         with a more personalized digital experience. If you are fine to continue
@@ -39,39 +40,15 @@ const CookieConsentBanner = () => {
       <div className="flex gap-4">
         <button
           onClick={handleDecline}
-          className="bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded flex items-center"
+          className="bg-gray-600 hover:bg-gray-700 min-w-36 text-white px-4 py-2 rounded flex items-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-1"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
-          I DECLINE
+          <CloseOutlined className="mr-2 text-xl" />I DECLINE
         </button>
         <button
           onClick={handleAccept}
-          className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded flex items-center"
+          className="bg-green-600 hover:bg-green-700 min-w-32 text-white px-4 py-2 rounded flex items-center"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-1"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          I AGREE
+          <CheckOutlined className="mr-2 text-xl" />I AGREE
         </button>
       </div>
     </div>
