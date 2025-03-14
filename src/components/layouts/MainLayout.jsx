15a692a90334239a9ebcb928ab8c1d650568488e
Shiv <shiv.choudhury@giva.co>
2025-03-14 19:27:47 +0530

    UI: Home Page improvements

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ export default function MainLayout(props) {
   return (
     <div>
       <Header />
-      {children}
+      <div className="container mx-auto p-4">{children}</div>
       <Footer />
     </div>
   );
