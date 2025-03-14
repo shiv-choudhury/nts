36fb93959583497f4bfb9a2ee1abd7d6cc99e891
Shiv <shiv.choudhury@giva.co>
2025-03-15 00:39:44 +0530

    UI: Side Menu - Add tabs and make main menu dynamic

--- src/components/layouts/SideMenu.jsx
+++ src/components/layouts/SideMenu.jsx
@@ -1,12 +1,16 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { CloseOutlined } from "@ant-design/icons";
+import useAppContext from "../context/UserContext";
+import { Link } from "react-router-dom";
 
-const SideMenu = (props) => {
-  const { isOpen, setIsOpen } = props;
+const SideMenu = ({ isOpen, setIsOpen }) => {
+  const { userState } = useAppContext();
+  const { headerData } = userState;
+
+  const [activeTab, setActiveTab] = useState("main");
 
   const toggleMenu = () => setIsOpen(!isOpen);
-
   const closeMenu = (e) => {
     if (!e.target.closest(".sidemenu")) {
       setIsOpen(false);
@@ -19,7 +23,6 @@ const SideMenu = (props) => {
     } else {
       document.body.classList.remove("overflow-hidden");
     }
-
     return () => document.body.classList.remove("overflow-hidden");
   }, [isOpen]);
 
@@ -44,58 +47,103 @@ const SideMenu = (props) => {
             exit={{ x: "-100%" }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
           >
-            {/* Header */}
-            <div className="p-4 flex justify-between items-center border-b border-gray-700">
-              <span className="text-lg font-bold">MAIN MENU</span>
-              <button className="text-white" onClick={toggleMenu}>
-                <CloseOutlined className="mr-2 text-md" />
+            {/* Header with Tabs */}
+            <div className="flex border-b border-gray-700">
+              <button
+                className={`flex-1 py-3 text-center ${
+                  activeTab === "categories"
+                    ? "text-blue-400 border-b-2 border-blue-400"
+                    : "text-gray-400 border-b-2 border-gray-900"
+                }`}
+                onClick={() => setActiveTab("categories")}
+              >
+                CATEGORIES
+              </button>
+              <button
+                className={`flex-1 py-3 text-center ${
+                  activeTab === "main"
+                    ? "text-blue-400 border-b-2 border-blue-400"
+                    : "text-gray-400 border-b-2 border-gray-900"
+                }`}
+                onClick={() => setActiveTab("main")}
+              >
+                MAIN MENU
               </button>
             </div>
 
-            {/* Scrollable Menu Items */}
+            {/* Content based on active tab */}
             <div className="flex-1 overflow-y-auto">
-              <ul className="px-4">
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  WALL
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  FLOOR
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  BATHROOM
-                </li>
-                <li className="pl-4 py-2 text-gray-400 hover:text-white">
-                  BATHROOM FLOOR TILES
-                </li>
-                <li className="pl-4 py-2 text-gray-400 hover:text-white">
-                  BATHROOM WALL TILES
-                </li>
-                <li className="pl-4 py-2 text-gray-400 hover:text-white">
-                  VICTORIAN / MOROCCAN TILES
-                </li>
-                <li className="pl-4 py-2 text-gray-400 hover:text-white">
-                  BATHROOM LUXURY TILES
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  PORCELAIN
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  WOOD EFFECT
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  OUTDOOR
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  ACCESSORIES
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-red-700 bg-red-600">
-                  CLEARANCE
-                </li>
-                <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
-                  REAL IMAGES
-                </li>
-                <li className="py-3 hover:bg-gray-800">CONTACT US</li>
-              </ul>
+              {activeTab === "main" ? (
+                <ul className="p-4">
+                  {headerData?.map((item, index) => (
+                    <li
+                      key={item.id || index}
+                      className="py-3 border-b border-gray-700 hover:bg-gray-800"
+                    >
+                      <Link
+                        className="hover:text-blue-600 block"
+                        key={item?.pageId?._id || index}
+                        to={item?.pageId?.pg_url_key}
+                        onClick={() => {
+                          setIsOpen(false);
+                        }}
+                      >
+                        {item.headerName}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <ul className="p-4">
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    WALL
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    FLOOR
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    BATHROOM
+                  </li>
+                  <li className="pl-4 py-2 text-gray-400 hover:text-white">
+                    BATHROOM FLOOR TILES
+                  </li>
+                  <li className="pl-4 py-2 text-gray-400 hover:text-white">
+                    BATHROOM WALL TILES
+                  </li>
+                  <li className="pl-4 py-2 text-gray-400 hover:text-white">
+                    VICTORIAN / MOROCCAN TILES
+                  </li>
+                  <li className="pl-4 py-2 text-gray-400 hover:text-white">
+                    BATHROOM LUXURY TILES
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    PORCELAIN
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    WOOD EFFECT
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    OUTDOOR
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    ACCESSORIES
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-red-700 bg-red-600">
+                    CLEARANCE
+                  </li>
+                  <li className="py-3 border-b border-gray-700 hover:bg-gray-800">
+                    REAL IMAGES
+                  </li>
+                  <li className="py-3 hover:bg-gray-800">CONTACT US</li>
+                </ul>
+              )}
+            </div>
+
+            {/* Close Button */}
+            <div className="p-4 border-t border-gray-700 flex justify-end">
+              <button className="text-white" onClick={toggleMenu}>
+                <CloseOutlined className="text-lg" />
+              </button>
             </div>
           </motion.div>
         </>
