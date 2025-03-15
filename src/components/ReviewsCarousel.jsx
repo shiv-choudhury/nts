8ee93e3ff2c130fcb20097602ad68ed5ab190ba8
Shiv <shiv.choudhury@giva.co>
2025-03-15 22:58:27 +0530

    UI: Fix review carousel issue

--- src/components/ReviewsCarousel.jsx
+++ src/components/ReviewsCarousel.jsx
@@ -7,17 +7,20 @@ import Loader from "../pages/Loader";
 const ReviewsCarousel = ({ reviewsData = [], stats = {}, loading }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [visibleReviews, setVisibleReviews] = useState(3);
+  let interval = null;
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex + 1 >= reviewsData.length ? 0 : prevIndex + 1
     );
+    clearInterval(interval);
   };
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex - 1 < 0 ? reviewsData.length - 1 : prevIndex - 1
     );
+    clearInterval(interval);
   };
 
   // Update visible reviews based on screen size
@@ -48,7 +51,7 @@ const ReviewsCarousel = ({ reviewsData = [], stats = {}, loading }) => {
       nextSlide();
     }, 5000);
     return () => clearInterval(interval);
-  }, []);
+  }, [currentIndex, reviewsData.length]);
 
   // Get the reviews to display based on currentIndex
   const getVisibleReviews = () => {
@@ -73,16 +76,7 @@ const ReviewsCarousel = ({ reviewsData = [], stats = {}, loading }) => {
               EXCELLENT
             </span>
             <div className="flex">
-              {[...Array(5)].map((_, i) => (
-                <svg
-                  key={i}
-                  className="w-4 h-4 md:w-5 md:h-5 text-orange-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
-              ))}
+              <ReviewStars rating={5} />
             </div>
           </div>
           <div className="flex items-center flex-wrap justify-center">
