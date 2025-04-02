18acf9479e76e846d5fdf1d210c27dd08db808e4
Shiv <shivprasadchoudhary1999@gmail.com>
2025-04-02 22:59:02 +0530

    UI: Implement wishlist page

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getFavoritesList } from "../apis/ApiCalls";
+import ProductCard from "../components/ProductCard";
 
 export default function Wishlist() {
-  return <div>Wishlist</div>;
+  const [wishlist, setWishlist] = useState([]);
+
+  useEffect(() => {
+    fetchWishlist();
+  }, []);
+
+  const fetchWishlist = async () => {
+    try {
+      const resp = await getFavoritesList();
+      const { favorites: data, success, message } = resp.data;
+
+      if (success) {
+        setWishlist(data);
+      } else {
+        toast.error(message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="mb-8 md:mb-12">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl md:text-2xl font-semibold text-blue-800">
+          Wishlist
+        </h2>
+      </div>
+
+      <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
+        {wishlist?.map((item) => (
+          <ProductCard data={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
