4a3fcba1178a8b21f88f8439e3489025b0e44c6e
Shiv <shivprasadchoudhary1999@gmail.com>
2025-04-05 20:36:02 +0530

    UI: Test tawkTo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
@@ -6,22 +7,20 @@ import Router from "./routes/Router";
 import "./App.css";
 import "primeicons/primeicons.css";
 import { UserContextProvider } from "./components/context/UserContext";
-import { useEffect } from "react";
-
-useEffect(() => {
-  const script = document.createElement("script");
-  script.async = true;
-  script.src = "https://embed.tawk.to/673afd382480f5b4f59fc918/1icv6qa48";
-  script.charset = "UTF-8";
-  script.setAttribute("crossorigin", "*");
-  document.body.appendChild(script);
-
-  return () => {
-    document.body.removeChild(script);
-  };
-}, []);
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://embed.tawk.to/673afd382480f5b4f59fc918/1icv6qa48";
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <>
       <BrowserRouter>
